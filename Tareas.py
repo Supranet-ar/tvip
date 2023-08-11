a9ea18fc1b786aa0549eae7c37f3d9d27a10a1d0
aguscano5 <canoa4450@gmail.com>
2023-08-11 11:03:59 -0300

    agregado de logica para configurar la hora actual de la tarea

--- Tareas.py
+++ Tareas.py
@@ -1,15 +1,31 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QDateTimeEdit, QPushButton
 from PyQt5.uic import loadUi
+from PyQt5.QtCore import QDateTime
 
 
 class VentanaPrincipal(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Ejemplo de ventana")
-
-        # Cargar la interfaz de Qt Designer desde el archivo .ui
         loadUi("interfaz/Tareas_2ventana.ui", self)
+
+        # Obtener la fecha y hora actual
+        current_datetime = QDateTime.currentDateTime()
+
+        # Configurar el QDateTimeEdit para mostrar la hora actual
+        self.dateTimeEdit.setDateTime(current_datetime)
+
+        # Conectar el botón con la función para programar la tarea
+        self.botonGuardar.clicked.connect(self.programar_tarea)
+
+    def programar_tarea(self):
+        # Obtener la fecha y hora seleccionada por el usuario
+        selected_datetime = self.dateTimeEdit.dateTime()
+
+        # Realizar la lógica de programación de la tarea aquí
+        # Por ejemplo, podrías imprimir la fecha y hora seleccionada
+        print("Tarea programada para:", selected_datetime.toString())
 
 
 if __name__ == "__main__":
@@ -19,4 +35,3 @@ if __name__ == "__main__":
     ventana.show()
 
     sys.exit(app.exec_())
-
