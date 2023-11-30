894abc49b9d96bf661d196c71b4ee3bb77fcc0b1
supra <supra912@gmail.com>
2023-11-30 09:01:07 -0300

    Revert "Tarea:Conexion login con main y solucion de frontend sin servidor activo"
    This reverts commit 2574e458448e63e0aa891e55bfcfe89d4ee5c890.

--- login.py
+++ login.py
@@ -1,105 +1,74 @@
-import subprocess
 import sys
 from PyQt5 import QtWidgets, uic
-import mysql.connector
-from mysql.connector import Error
+import subprocess
 
-class VentanaLogin(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
 
-        # Carga la interfaz de archivo.ui creado en Qt Designer
-        self.window = uic.loadUi("interfaz/ventana_login.ui")
+# Carga la interfaz de archivo.ui creado en Qt Designer
+app = QtWidgets.QApplication(sys.argv)
+window = uic.loadUi("interfaz/ventana_login.ui")
 
-        # Carga la interfaz de login exitoso y login error
-        self.ventana_exito = uic.loadUi("interfaz/ventana_login_exito.ui")
-        self.ventana_error = uic.loadUi("interfaz/ventana_login_error.ui")
+# Carga la interfaz de login exitoso y login error
+ventana_exito = uic.loadUi("interfaz/ventana_login_exito.ui")
+ventana_error = uic.loadUi("interfaz/ventana_login_error.ui")
 
-        # Conecta la función verificar_usuario al botón de inicio de sesión
-        self.window.iniciarSesionButton.clicked.connect(self.verificar_usuario)
+# Función para verificar el usuario y la contraseña
+def verificar_usuario():
+    usuario = window.usuarioLineEdit.text()
+    contrasena = window.contrasenaLineEdit.text()
 
-        # Conecta la función cerrar_ventana_exito al botón "Salir" de la ventana de éxito
-        self.ventana_exito.salirButton.clicked.connect(self.cerrar)
+    if usuario == "tvip" and contrasena == "1234":
+        mostrar_interfaz_exito()
+    else:
+        mostrar_interfaz_error()
 
-        # Conecta la función cerrar_ventana_error al botón "Salir" de la ventana de error
-        self.ventana_error.salirButton.clicked.connect(self.cerrar)
+# Función para mostrar la interfaz de login exitoso
+def mostrar_interfaz_exito():
+    ventana_exito.show()  # Muestra la interfaz de login exitoso
+    window.close()  # Cierra la interfaz de login principal
 
-        # Conecta la función mostrar_interfaz_login al botón "Intentar nuevamente" de la ventana de error
-        self.ventana_error.intentarNuevamenteButton.clicked.connect(self.mostrar_interfaz_login)
+# Función para mostrar la interfaz de login error
+def mostrar_interfaz_error():
+    ventana_error.show()  # Muestra la interfaz de login error
+    window.close()  # Cierra la interfaz de login principal
 
-        # Conecta la función abrir_habitaciones al botón "OK" de la ventana de éxito
-        self.ventana_exito.okButton.clicked.connect(self.abrir_habitaciones)
+# Función para mostrar la interfaz de login
+def mostrar_interfaz_login():
+    ventana_error.close()  # Cierra la interfaz de login error
+    window.show()  # Muestra la interfaz de login principal
 
-        # Muestra la interfaz de login principal
-        self.window.show()
+# Función para cerrar la ventana
+def cerrar():
+    window.close()
+    ventana_exito.close()
+    ventana_error.close()
 
-    def verificar_usuario(self):
-        print("Verificando usuario...")
-        usuario = self.window.Username2.text()
-        contrasena = self.window.Pasword.text()
-
-        print(f"Usuario ingresado: {usuario}")
-        print(f"Contraseña ingresada: {contrasena}")
-
-        if self.validar_credenciales(usuario, contrasena):
-            print("Inicio de sesión exitoso!")
-            self.abrir_habitaciones()
-        else:
-            print("Inicio de sesión fallido.")
-            self.mostrar_interfaz_error()
-
-    def validar_credenciales(self, usuario, contrasena):
-        try:
-            # Conectar a la base de datos MySQL
-            connection = mysql.connector.connect(
-                host='192.168.100.90',
-                database='tvip',
-                user='tvip',
-                password='3434'
-            )
-
-            # Crear un cursor para ejecutar consultas
-            cursor = connection.cursor()
-
-            # Consulta para verificar las credenciales
-            query = "SELECT * FROM usuarios WHERE username = %s AND password = %s"
-            cursor.execute(query, (usuario, contrasena))
-
-            # Obtener el resultado de la consulta
-            result = cursor.fetchone()
-
-            # Cerrar el cursor y la conexión
-            cursor.close()
-            connection.close()
-
-            # Verificar si se encontraron credenciales válidas
-            return result is not None
-
-        except Error as e:
-            print("Error al conectar a la base de datos:", e)
-            return False
-
-    def mostrar_interfaz_exito(self):
-        self.ventana_exito.show()  # Muestra la interfaz de login exitoso
-        self.window.close()  # Cierra la interfaz de login principal
-
-    def mostrar_interfaz_error(self):
-        self.ventana_error.show()  # Muestra la interfaz de login error
-        self.window.close()  # Cierra la interfaz de login principal
-
-    def mostrar_interfaz_login(self):
-        self.ventana_error.close()  # Cierra la interfaz de login error
-        self.window.show()  # Muestra la interfaz de login principal
-
-    def cerrar(self):
-        self.window.close()
-        self.ventana_exito.close()
-        self.ventana_error.close()
-
-    def abrir_habitaciones(self):
+    # Función para abrir el archivo "panel.py"
+def abrir_habitaciones():
+        ventana_exito.close()
         subprocess.run(["python", "main.py"])
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication([])
-    ventana_login = VentanaLogin()
-    sys.exit(app.exec_())
+# Conecta la función verificar_usuario al botón de inicio de sesión
+window.iniciarSesionButton.clicked.connect(verificar_usuario)
+
+# Conecta la función cerrar_ventana al botón de salir
+window.salirButton.clicked.connect(cerrar)
+
+# Conecta la función cerrar_ventana_exito al botón "Salir" de la ventana de éxito
+ventana_exito.salirButton.clicked.connect(cerrar)
+
+# Conecta la función cerrar_ventana_error al botón "Salir" de la ventana de error
+ventana_error.salirButton.clicked.connect(cerrar)
+
+# Conecta la función mostrar_interfaz_login al botón "Intentar nuevamente" de la ventana de error
+ventana_error.intentarNuevamenteButton.clicked.connect(mostrar_interfaz_login)
+
+# Conecta la función abrir_habitaciones al botón "OK" de la ventana de éxito
+ventana_exito.okButton.clicked.connect(abrir_habitaciones)
+
+# Muestra la interfaz de login principal
+window.show()
+sys.exit(app.exec_())
+
+# Muestra la interfaz
+window.show()
+sys.exit(app.exec_())
